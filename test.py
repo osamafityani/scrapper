3b99db6f1f65ced1ddaf346e53a55336d5119649
malek aldebsi <malek315@gmail.com>
2023-08-22 22:12:46 +0300

    ...

---
+++ test.py
@@ -0,0 +1,15 @@
+import requests
+
+# URL you want to send the GET request to
+url = 'http://127.0.0.1:8000/scrap_home_depot/'
+
+# Sending the GET request
+response = requests.get(url, timeout=15)
+
+# Check the status code of the response
+if response.status_code == 200:
+    print("Request successful")
+    # Print the content of the response
+    print(response.text)
+else:
+    print(f"Request failed with status code: {response.status_code}")
