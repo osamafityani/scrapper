cc13dcf198df46938a9c841d27c1e8e0385efe22
Haitham-Osama <111170029+Haitham-Osama@users.noreply.github.com>
2023-09-14 15:28:07 +0300

    ...

--- ulta/views.py
+++ ulta/views.py
@@ -1,3 +1,5 @@
+from concurrent.futures import ThreadPoolExecutor
+import concurrent.futures
 import time
 from decimal import Decimal
 
@@ -196,3 +198,116 @@ def item_data(request):
         print('************************************')
 
     return Response()
+
+
+def make_request(url):
+    
+    try:
+        session = requests.Session()
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
+        }
+        response = session.get(url,headers=headers)
+        data = response.text
+        soup = BeautifulSoup(data, 'xml')
+        with open('items.txt', 'a') as file:
+            for loc in soup.find_all('loc'):
+                if not loc.text.startswith('https://media'):
+                    file.write(loc.text + '\n')
+            file.close()
+        
+        session.close()
+        return f"Response from {url}: {response.status_code}\n"
+    except Exception as e:
+        return f"Error while requesting {url}: {e}\n"
+
+
+def create_products(url):
+    headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
+        }
+    try:
+        session = requests.Session()
+        response = session.get(url,headers=headers)
+        data = response.text
+        soup = BeautifulSoup(data, 'html.parser')
+
+        price = soup.find('span', class_='Text-ds Text-ds--title-6 Text-ds--left Text-ds--black').text
+
+        if price.count('$') == 1:
+            price = Decimal(price[1:])
+        else:
+            price = 0
+
+        try:
+            product, created = Product.objects.get_or_create(link=url)
+        except:
+            print('+++++++++++++++++++++++++++++++++')
+
+        if created:
+            product.current_price = price
+
+        else:
+            product.last_price = product.current_price
+            product.current_price = price
+
+        try:
+            product.save()
+        except:
+            print('**********************************')
+        session.close()
+        return f"Response from {url}: {response.status_code}\n"
+    except Exception as e:
+        return f"Error while requesting {url}: {e}\n"
+
+def read_sitemap_urls(sitemap_url):
+    headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
+        }
+    response = requests.get(sitemap_url,headers=headers)
+    soup = BeautifulSoup(response.content, 'xml')
+    return [loc.text for loc in soup.find_all('loc')]
+
+
+def read_urls_chunk(file, chunk_size=1000):
+    urls = []
+    for _ in range(chunk_size):
+        line = file.readline()
+        if not line:
+            break
+        urls.append(line.strip())
+    return urls
+
+
+def threaded_requests(request):
+    with open('items.txt', 'w') as file:
+        file.truncate(0)
+
+    start_time = time.time()
+    sitemap_url = 'https://www.ulta.com/sitemap/p.xml'
+  
+    # Read URLs from the sitemap
+    urls = read_sitemap_urls(sitemap_url)
+
+    
+    with ThreadPoolExecutor(max_workers=None) as executor: # optimally defined number of threads
+        res = [executor.submit(make_request, url) for url in urls]
+        concurrent.futures.wait(res)
+
+    with open('items.txt', 'r') as file:
+        while True:
+            urls_chunk = read_urls_chunk(file)
+            if not urls_chunk:
+                break
+
+            with ThreadPoolExecutor(max_workers=None) as executor:
+                res = [executor.submit(create_products, url) for url in urls_chunk]
+                concurrent.futures.wait(res)
+
+    # results = [future.result() for future in res]
+
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    # results.insert(0,elapsed_time)
+    return HttpResponse(elapsed_time)
